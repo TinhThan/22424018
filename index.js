29b4d2a133caf0365a21212613622bc6db85f82d
22424018 <22424018@student.hcmus.edu.vn>
2023-08-19 22:15:26 +0700

    Test Jenkins

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, this is a Node.js server, 22424018!\n');
+  res.end('Hello, this is a Node.js server, 22424018 - Thân Văn Đức Tính!\n');
 });
 
 const PORT = 3000;
