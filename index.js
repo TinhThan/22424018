2c0ece9ccac5107c1e8f65b97ba465c6af000ad0
22424018 <22424018@student.hcmus.edu.vn>
2023-08-21 20:32:04 +0700

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, this is a Node.js server, 22424018 - Thân Văn Đức Tính!\n');
+  res.end('Hello, this is a Node.js server, 22424018 - THAN VAN DUC TINH!\n');
 });
 
 const PORT = 3000;
